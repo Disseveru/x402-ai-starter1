d394cf3c0d58a251acf41adf50dd39685808d118
Disseveru <167385081+Disseveru@users.noreply.github.com>
2026-05-11 04:37:19 -0700

    Disable ignoreBuildErrors and ignoreDuringBuilds for production readiness

--- next.config.ts
+++ next.config.ts
@@ -6,12 +6,12 @@ if (!process.env.CI) {
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
 };
 
 export default nextConfig;
